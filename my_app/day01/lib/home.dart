cee73af721c3397fe9c18c09d5db9d28d4eefe8d
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-10-19 01:27:06 +0900

    ➕ level 2

--- my_app/day01/lib/home.dart
+++ my_app/day01/lib/home.dart
@@ -12,53 +12,66 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
+      body: Stack(
         children: [
-          Expanded(
-            flex: 1,
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Row(
-                    children: [
-                      Expanded(
-                          flex: 1,
-                          child: Column(children: const [
-                            Area(color: Colors.grey),
-                            Area(color: Colors.orange),
-                            Area(color: Colors.blue),
-                            Area(color: Colors.pink),
-                          ])),
-                      Expanded(
-                          flex: 2,
-                          child: Column(children: [
-                            Area(flex: 3, color: Colors.blue.shade400),
-                            Expanded(
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Row(
+                        children: [
+                          Expanded(
                               flex: 1,
-                              child: Row(
-                                children: [
-                                  Area(color: Colors.teal.shade100),
-                                  const Area(color: Colors.yellow)
-                                ],
-                              ),
-                            )
-                          ])),
-                    ],
-                  ),
+                              child: Column(children: const [
+                                Area(color: Colors.grey),
+                                Area(color: Colors.orange),
+                                Area(color: Colors.blue),
+                                Area(color: Colors.pink),
+                              ])),
+                          Expanded(
+                              flex: 2,
+                              child: Column(children: [
+                                Area(flex: 3, color: Colors.blue.shade400),
+                                Expanded(
+                                  flex: 1,
+                                  child: Row(
+                                    children: [
+                                      Area(color: Colors.teal.shade100),
+                                      const Area(color: Colors.yellow)
+                                    ],
+                                  ),
+                                )
+                              ])),
+                        ],
+                      ),
+                    ),
+                    const Area(flex: 3, color: Colors.black),
+                    const Area(flex: 3, color: Colors.yellow),
+                    const Area(flex: 3),
+                  ],
                 ),
-                const Area(flex: 3, color: Colors.black),
-                const Area(flex: 3, color: Colors.yellow),
-                const Area(flex: 3),
-              ],
+              ),
+              const Area(
+                flex: 2,
+                color: Colors.pink,
+                margin: EdgeInsets.only(right: 10),
+              ),
+              const Area(flex: 1, color: Colors.pink),
+            ],
+          ),
+          Positioned(
+            top: 100,
+            left: 40,
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.blueGrey,
             ),
           ),
-          const Area(
-            flex: 2,
-            color: Colors.pink,
-            margin: EdgeInsets.only(right: 10),
-          ),
-          const Area(flex: 1, color: Colors.pink),
         ],
       ),
       floatingActionButton: FloatingActionButton(
