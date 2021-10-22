590b9e3cfde606dbd393372fe5fc86207c74aba7
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-10-22 15:03:27 +0900

    🔧 fix 2c

--- day01/lib/problem_2c.dart
+++ day01/lib/problem_2c.dart
@@ -48,6 +48,7 @@ class _Problem2CState extends State<Problem2C> {
       // body: SafeArea(child: buildContent(8, 4)), // error ??
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: buildContent2(8, 4),
         ),
       ),
@@ -56,11 +57,24 @@ class _Problem2CState extends State<Problem2C> {
 
   List<Widget> buildContent2(int row, int column) {
     List<Widget> lst = [];
-    for (var i = 0; i < row; i++) {
-      lst.add(Row(
-        children: buildRow(column: column),
-      ));
-      // lst.add(const SizedBox(height: 5));
+    if (row > 0 && column > 0) {
+      for (var i = 0; i < row; i++) {
+        lst.add(Expanded(
+          child: Row(
+            children: buildRow(column: column),
+          ),
+        ));
+        lst.add(const SizedBox(height: 5));
+      }
+    } else {
+      lst.add(
+        const Center(
+          child: Text(
+            'Can not build',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      );
     }
     return lst;
   }
