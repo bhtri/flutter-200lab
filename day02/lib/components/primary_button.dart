a405a65d174f3bf48c9f7d22bbb12659f8c797fe
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-10-28 20:07:33 +0900

    ➕ button

---
+++ day02/lib/components/primary_button.dart
@@ -0,0 +1,32 @@
+import 'package:day02/constant/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  const PrimaryButton({
+    Key? key,
+    required this.text,
+    required this.press,
+    required this.color,
+    this.padding = const EdgeInsets.all(AppStyles.kDefaultPadding * 0.75),
+    required this.textStyle,
+  }) : super(key: key);
+
+  final String text;
+  final VoidCallback? press;
+  final Color color;
+  final EdgeInsets padding;
+  final TextStyle? textStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      minWidth: double.infinity,
+      onPressed: press,
+      color: color,
+      padding: padding,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(40))),
+      child: Text(text, style: textStyle),
+    );
+  }
+}
--- day02/lib/constant/app_styles.dart
+++ day02/lib/constant/app_styles.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 class AppStyles {
   static const TextStyle h3 = TextStyle(fontSize: 11, color: Colors.white);
   static const TextStyle h2 = TextStyle(fontSize: 11, color: Colors.white);
+
+  static const kDefaultPadding = 20.0;
 }
